01f82771d0352c77dd3c697b8f523278a610b60f
Richard PYT <rick.info.rsoc@gmail.com>
2021-01-11 18:30:25 +0100

    Début Mise en place Design pattern Composite sur IPatern + ajout GenericList + autres...

--- parser.ts
+++ parser.ts
@@ -1,206 +1,512 @@
-export interface IElement {
-
-    canHaveChild(): boolean;
-    extractStartMarker(stringToParse: IStringToParse): string | null;
-    extractEndMarker(stringToParse: IStringToParse): string | null;
+import { IStringComparator } from './_modules/general/modeles/interfaces';
+import { StringComparator } from './_modules/general/modeles/StringComparator';
+import { GenericList } from './_modules';
 
 
-    getPossibleSiblingElements(): Array<IElement>;
-}
+export interface IPattern {
+    getMatchingStringFromStringToParse(stringToParse: string): string;
 
-export interface ISimpleElement extends IElement {
-
-}
-
-export interface IBlockElement extends IElement {
-
-}
-
-
-export abstract class AElement {
-    protected abstract beginsWithStartMarker(stringToParse: IStringToParse): boolean;
-    protected abstract beginsWithEndMarker(stringToParse: IStringToParse): boolean;
-
-    extractStartMarker(stringToParse: IStringToParse): boolean {
-        const retour: boolean = this.beginsWithStartMarker(stringToParse);
-        if (stringToParse.) {
-
-        }
-    }
-    extractEndMarker(stringToParse: IStringToParse): boolean;
+    setMinOccurencesNumber(minOccurencesNumber: number): IPattern;
+    getMinOccurencesNumber(): number;
+    setMaxOccurencesNumber(maxOccurencesNumber: number): IPattern;
+    getMaxOccurencesNumber(): number;
+    isDefinedMaxOccurencesNumber(): boolean;
+    setOccurencesNumbers(minOccurencesNumber: number, maxOccurencesNumber?: number): IPattern;    
 
 }
 
-/*
-export class SimpleElement extends AElement implements IElement {
-    canHaveChild(): boolean {
-        return(false);
-    }
+export interface ISimplePattern extends IPattern {
+    setString(string: string): ISimplePattern;
+    getString(): string;
+
+    setCaseSensitivity(caseSensitivity: boolean): ISimplePattern;
+    isCaseSensitivity(): boolean;
 }
 
-export class CompositeElement extends AElement implements IElement {
-    canHaveChild(): boolean {
-        return(true);
-    }
-}
-*/
+export abstract class APattern implements IPattern {
 
-export interface ITreeItem {
+    private static readonly PATTERN_MAX_OCCURENCES_NUMBER: number = 50;
+    private static readonly PATTERN_MAX_OCCURENCES_NUMBER_NOT_DEFINED: number = null;
 
-}
-
-export interface ITreeSimpleItem extends ITreeItem {
-
-}
-
-export interface ITreeBlockItem extends ITreeItem {
-
-}
-
-export abstract class ATreeItem {
-
-}
-
-export abstract class ATreeSimpleItem extends ATreeItem {
-
-}
-
-export abstract class ATreeBlockItem extends ATreeItem {
-
-}
-
-export class TreeSimpleItem extends ATreeSimpleItem implements ITreeItem {
-}
-export class TreeBlockItem extends ATreeBlockItem implements ITreeItem {
-}
-
-export class TreeRootBlockItem extends TreeBlockItem implements ITreeItem {
-
-}
-
-export interface ITreeItemFactory {
-    // createBlock()
-}
-
-
-export interface ITreeBuilder {
-
-    addElement(element: IElement): ITreeBuilder;
-
-}
-
-export abstract class ATreeBuilder /*implements ITreeBuilder*/ {
-
-    private currentBlockItem: ITreeBlockItem;
+    private minOccurencesNumber: number = 1;
+    private maxOccurencesNumber: number = APattern.PATTERN_MAX_OCCURENCES_NUMBER_NOT_DEFINED;
 
     constructor() {
-        this.currentBlockItem = new TreeRootBlockItem();
+    }
+
+    abstract getMatchingStringFromStringToParse(stringToParse: string): string;
+       
+
+    setMinOccurencesNumber(minOccurencesNumber: number): IPattern {
+        this.minOccurencesNumber = minOccurencesNumber;
+        this.checkValidNbOccurences();
+        return (this);
+    }
+    getMinOccurencesNumber(): number {
+        return (this.minOccurencesNumber);
+    }
+
+    setMaxOccurencesNumber(maxOccurencesNumber: number): IPattern {
+        this.minOccurencesNumber = maxOccurencesNumber;
+        this.checkValidNbOccurences();
+        return (this);
+    }
+    getMaxOccurencesNumber(): number {
+        return (this.maxOccurencesNumber);
+    }
+    setOccurencesNumbers(minOccurencesNumber: number, maxOccurencesNumber: number = APattern.PATTERN_MAX_OCCURENCES_NUMBER_NOT_DEFINED): IPattern {
+        this.setMinOccurencesNumber(minOccurencesNumber);
+        this.setMaxOccurencesNumber(maxOccurencesNumber);
+        return (this);
     }
 
 
-    addElement(element: IElement): ITreeBuilder {
-        if (element.canHaveChild()) {
-            this.addBlockElement(element);
+    isDefinedMaxOccurencesNumber(): boolean {
+        const result: boolean = (this.maxOccurencesNumber !== APattern.PATTERN_MAX_OCCURENCES_NUMBER_NOT_DEFINED);
+        return (result);
+    }
 
-        } else {
-            this.addSimpleElement(element);
+
+    private checkValidNbOccurences(): void {
+        const ok: boolean = this.testValidNbOccurences();
+        if (!ok) {
+            throw new Error(`Nombre d'occurences min=${this.minOccurencesNumber} ou max=${this.maxOccurencesNumber}, invalide.`);
         }
-        return (this);
+    }
+    private testValidNbOccurences(): boolean {
+        const result: boolean = (
+            (this.minOccurencesNumber >= 0)
+            &&
+            (this.maxOccurencesNumber <= APattern.PATTERN_MAX_OCCURENCES_NUMBER)
+            &&
+            (this.maxOccurencesNumber >= this.minOccurencesNumber)
+        );
+        return (result);
     }
 
-    p
 }
 
 
+export abstract class ASimplePattern extends APattern implements ISimplePattern {
+    private static defaultCaseSensitivity: boolean = true;
+    
+    private string: string;
+    private caseSensitivity: boolean;
 
+    
+    static setDefaultCaseSensitivity(caseSensitivity: boolean): void {
+        this.defaultCaseSensitivity = caseSensitivity;
+    }
+    
+    constructor(string: string = "", caseSensitivity: boolean = ASimplePattern.defaultCaseSensitivity) {
+        super();
 
-export interface IStringParser {
-    setString(stringToParse: string): IStringParser;
+        this.setString(string);
+        this.setCaseSensitivity(caseSensitivity);
+    }
+    
+    setString(string: string): ISimplePattern {
+        if (string !== null) {
+            this.string = string;
+        }
+        return(this);
+    }
+    getString(): string {
+        return(this.string);
+    }
+
+    setCaseSensitivity(caseSensitivity: boolean): ISimplePattern {
+        this.caseSensitivity = caseSensitivity;
+        return(this);
+    }
+    isCaseSensitivity(): boolean {
+        return(this.caseSensitivity);
+    }    
 }
 
 
+export interface IStringPattern extends ISimplePattern {
+    setMatchComparator(matchComparator: IStringComparator): IStringPattern;
+}
 
+export interface IRegExpStringPattern extends ISimplePattern {
+}
 
-export abstract class AStringParser /*implements IStringParser*/ {
+export interface IStringPattern extends IPattern {
+    setMatchComparator(matchComparator: IStringComparator): IStringPattern;
+}
+export class StringPattern extends ASimplePattern implements IStringPattern {
 
-    private treeBuilder: ITreeBuilder;
-    private string: IStringToParse;
+    private matchComparator: IStringComparator = null;
 
-    constructor(treeBuilder: ITreeBuilder, stringToParse: string = "") {
-        this.setTreeBuilder(treeBuilder);
-        this.setString(stringToParse);
+    setMatchComparator(matchComparator: IStringComparator): IStringPattern {
+        this.matchComparator = matchComparator;
+        return(this);
+    }
+    private getMatchComparator(): IStringComparator {
+        if (this.matchComparator === null) {
+            this.matchComparator = this.createStringComparator();
+        }
+        return(this.matchComparator);
+    }
+    private createStringComparator(): IStringComparator {
+        const result: IStringComparator = new StringComparator();
+        return(result);
     }
 
-    setString(stringToParse: string): IStringParser {
-        this.string = this.createStringToParseObject(stringToParse);
-        return (this);
+    getMatchingStringFromStringToParse(stringToParse: string): string {
+        let result: string = "";
+
+        const string: string = this.getString();
+        if ((string !== "") && (stringToParse.length >= string.length)) {
+
+            const stringToParseStart: string = stringToParse.substr(0, string.length);
+            const match: boolean = this.getMatchComparator()
+                                    .setCaseSensitivity(this.isCaseSensitivity())
+                                    .testEquality(string, stringToParseStart);
+
+            result = (match) ? stringToParseStart : "";
+
+        }
+
+        return (result);
+    }
+}
+
+
+export class RegExpStringPattern extends ASimplePattern implements IRegExpStringPattern {
+
+    getMatchingStringFromStringToParse(stringToParse: string): string {
+        let result: string = "";
+
+        const string: string = this.getString();
+        if ((string !== "") && (stringToParse.length > 0)) {
+
+            const regExp: RegExp = this.getRegExp();
+            const match: Array<string> = stringToParse.match(regExp);
+
+            result = (match !== null) ? match[0] : "";
+
+        }
+
+        return (result);
     }
 
-    private setTreeBuilder(treeBuilder: ITreeBuilder): IStringParser {
-        this.treeBuilder = treeBuilder;
-        return (this);
+    private getRegExp(): RegExp {
+        const regExpOption: string = (this.isCaseSensitivity()) ? "i" : "";
+        const regExpString: string = this.getString();
+        const result: RegExp = new RegExp(`^${regExpString}`, regExpOption);
+        return (result);
+    }    
+
+}
+
+export interface IPatternsList {
+    definePatterns(patterns: Array<IPattern>): IPatternsList;
+    definePattern(pattern: IPattern): IPatternsList;
+    addPatterns(patterns: Array<IPattern>): IPatternsList;
+    addPattern(pattern: IPattern): IPatternsList;    
+}
+
+export abstract class APatternsList extends APattern implements IPattern, IPatternsList {
+
+    protected list: GenericList<IPattern>;
+
+    constructor() {
+        super();
+
+        this.list = new GenericList<IPattern>();
+        this.list.setAllowNullElement(false);
+    }
+    
+    definePatterns(patterns: Array<IPattern>): IPatternsList {
+        if (patterns !== null) {
+            this.list.clear();
+            this.addPatterns(patterns);
+        }
+        return(this);
     }
 
-    private createStringToParseObject(stringToParse: string): IStringToParse {
-        const retour: IStringToParse = new StringToParse(stringToParse);
-        return (retour);
+    definePattern(pattern: IPattern): IPatternsList {
+        if (pattern !== null) {
+            this.definePatterns(Array(pattern));
+        }
+        return(this);         
     }
 
-    xxx(): void {
-        let element: IElement;
-        let foundElementMatching: boolean = false;
+    addPatterns(patterns: Array<IPattern>): IPatternsList {
+        if (patterns !== null) {
+            this.list.addElements(patterns);
+        }
+        return(this);        
+    }
 
-        while (!this.string.isPointerAtTheEnd()) {
+    addPattern(pattern: IPattern): IPatternsList {
+        if (pattern !== null) {
+            this.list.addElement(pattern);
+        }
+        return(this); 
+    }    
 
-            foundElementMatching = false;
-            while ((element = this.getNextPossibleElement()) !== null) {
+}
 
-                if (element.isBloc
-                si element match as start marker
-                    if this.curret
+export abstract class OrderedPatternsList extends APatternsList {
+    
+    protected abstract mustStopSearchingMatching(matchingStringFromStringToParse: string): boolean;
 
-                foundElementMatching = this.testElementMatching(element);
-                if (foundElementMatching) {
-                    this.currentBlock.addElement(element);
-                    break;
+    getMatchingStringFromStringToParse(stringToParse: string): string {
+        const result:string = "";
+
+        this.list.each<string>(
+            (pattern: IPattern, index: number): string => {
+                const matchingStringFromStringToParse: string = pattern.getMatchingStringFromStringToParse(stringToParse);
+                return(matchingStringFromStringToParse);
+            },
+
+            (matchingStringFromStringToParse: string): void => {
+                if (matchingStringFromStringToParse !== "") {
+                    this.treatMatchingStringFromStringToParse(matchingStringFromStringToParse, result);
                 }
-            }
+            },
 
-            if (!foundElementMatching) {
-                this.string.incrementPointerPosition(1);
+            (matchingStringFromStringToParse: string): boolean => {
+                let breakLoop: boolean;
+                breakLoop = this.mustStopSearchingMatching(matchingStringFromStringToParse);
+                return(breakLoop);
             }
+        );
 
-        }
+        return(result);
+    }
+
+    private treatMatchingStringFromStringToParse(
+        matchingStringFromStringToParse: string,
+        resultToUpdate: string
+    ): void {
 
     }
 
+}
 
-    private testElementMatching(element: IElement): boolean {
+export class OrderedFullMatchPatternsList extends OrderedPatternsList {
+    mustStopSearchingMatching(matchingStringFromStringToParse: string): boolean {
+        const result: boolean = (matchingStringFromStringToParse === "");
+        return(result);
+    }
+}
 
-        let retour: boolean;
+export class OrderedOneMatchPatternsList extends OrderedPatternsList {
+    mustStopSearchingMatching(matchingStringFromStringToParse: string): boolean {
+        const result: boolean = (matchingStringFromStringToParse !== "");
+        return(result);
+    }
+}
 
-        let foundMarker: string;
 
-        if ((foundMarker = element.extractStartMarker(this.string)) !== null) {
+//***********************************************************
+// export interface IElement {
+
+//     canHaveChild(): boolean;
+//     extractStartMarker(stringToParse: IStringToParse): string | null;
+//     extractEndMarker(stringToParse: IStringToParse): string | null;
+
+
+//     getPossibleSiblingElements(): Array<IElement>;
+// }
+
+// export interface ISimpleElement extends IElement {
+
+// }
+
+// export interface IBlockElement extends IElement {
+
+// }
+
+
+// export abstract class AElement {
+//     protected abstract beginsWithStartMarker(stringToParse: IStringToParse): boolean;
+//     protected abstract beginsWithEndMarker(stringToParse: IStringToParse): boolean;
+
+//     extractStartMarker(stringToParse: IStringToParse): boolean {
+//         const retour: boolean = this.beginsWithStartMarker(stringToParse);
+//         if (stringToParse.) {
+
+//         }
+//     }
+//     extractEndMarker(stringToParse: IStringToParse): boolean;
+
+// }
+
+// /*
+// export class SimpleElement extends AElement implements IElement {
+//     canHaveChild(): boolean {
+//         return(false);
+//     }
+// }
+
+// export class CompositeElement extends AElement implements IElement {
+//     canHaveChild(): boolean {
+//         return(true);
+//     }
+// }
+// */
+
+// export interface ITreeItem {
+
+// }
+
+// export interface ITreeSimpleItem extends ITreeItem {
+
+// }
+
+// export interface ITreeBlockItem extends ITreeItem {
+
+// }
+
+// export abstract class ATreeItem {
+
+// }
+
+// export abstract class ATreeSimpleItem extends ATreeItem {
+
+// }
+
+// export abstract class ATreeBlockItem extends ATreeItem {
+
+// }
+
+// export class TreeSimpleItem extends ATreeSimpleItem implements ITreeItem {
+// }
+// export class TreeBlockItem extends ATreeBlockItem implements ITreeItem {
+// }
+
+// export class TreeRootBlockItem extends TreeBlockItem implements ITreeItem {
+
+// }
+
+// export interface ITreeItemFactory {
+//     // createBlock()
+// }
+
+
+// export interface ITreeBuilder {
+
+//     addElement(element: IElement): ITreeBuilder;
+
+// }
+
+// export abstract class ATreeBuilder /*implements ITreeBuilder*/ {
+
+//     private currentBlockItem: ITreeBlockItem;
+
+//     constructor() {
+//         this.currentBlockItem = new TreeRootBlockItem();
+//     }
+
+
+//     addElement(element: IElement): ITreeBuilder {
+//         if (element.canHaveChild()) {
+//             this.addBlockElement(element);
+
+//         } else {
+//             this.addSimpleElement(element);
+//         }
+//         return (this);
+//     }
+
+//     p
+// }
+
+
+
+
+// export interface IStringParser {
+//     setString(stringToParse: string): IStringParser;
+// }
+
+
+
+
+// export abstract class AStringParser /*implements IStringParser*/ {
+
+//     private treeBuilder: ITreeBuilder;
+//     private string: IStringToParse;
+
+//     constructor(treeBuilder: ITreeBuilder, stringToParse: string = "") {
+//         this.setTreeBuilder(treeBuilder);
+//         this.setString(stringToParse);
+//     }
+
+//     setString(stringToParse: string): IStringParser {
+//         this.string = this.createStringToParseObject(stringToParse);
+//         return (this);
+//     }
+
+//     private setTreeBuilder(treeBuilder: ITreeBuilder): IStringParser {
+//         this.treeBuilder = treeBuilder;
+//         return (this);
+//     }
+
+//     private createStringToParseObject(stringToParse: string): IStringToParse {
+//         const retour: IStringToParse = new StringToParse(stringToParse);
+//         return (retour);
+//     }
+
+//     xxx(): void {
+//         let element: IElement;
+//         let foundElementMatching: boolean = false;
+
+//         while (!this.string.isPointerAtTheEnd()) {
+
+//             foundElementMatching = false;
+//             while ((element = this.getNextPossibleElement()) !== null) {
+
+//                 if (element.isBloc
+//                 si element match as start marker
+//                     if this.curret
+
+//                 foundElementMatching = this.testElementMatching(element);
+//                 if (foundElementMatching) {
+//                     this.currentBlock.addElement(element);
+//                     break;
+//                 }
+//             }
+
+//             if (!foundElementMatching) {
+//                 this.string.incrementPointerPosition(1);
+//             }
+
+//         }
+
+//     }
+
+
+//     private testElementMatching(element: IElement): boolean {
+
+//         let retour: boolean;
+
+//         let foundMarker: string;
+
+//         if ((foundMarker = element.extractStartMarker(this.string)) !== null) {
             
 
-        } else if ((foundMarker = element.extractEndMarker(this.string)) !== null) {
-            this.treeBuilder.addElement(element);
+//         } else if ((foundMarker = element.extractEndMarker(this.string)) !== null) {
+//             this.treeBuilder.addElement(element);
 
-        }
+//         }
 
-        retour = (foundMarker !== null);
-        if (retour) {
-            this.string.incrementPointerPosition(foundMarker.length);
+//         retour = (foundMarker !== null);
+//         if (retour) {
+//             this.string.incrementPointerPosition(foundMarker.length);
 
-        }
+//         }
 
-        return (retour);
-    }
+//         return (retour);
+//     }
 
-    private getNextPossibleElement(): IElement {
+//     private getNextPossibleElement(): IElement {
 
-    }
+//     }
 
-}
+// }
 
