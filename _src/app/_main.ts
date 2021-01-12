e6eeb9cec8a1ed9658548c302dc17a295b29dd66
Richard PYT <rick.info.rsoc@gmail.com>
2021-01-12 08:58:27 +0100

    Tentative d'exec. basique dans l'environnement: souci de chemins dans les .js de _dist/, il fallait s'y attendre, je vais  donc passer en projet Angular !

--- _src/app/_main.ts
+++ _src/app/_main.ts
@@ -5,9 +5,25 @@
                                             //Mais bon, c'est pas très beau non plus :/
                                             //Intérêt: dans le code compilé (dist/...)
                                             //         on aura bien l'extension ".js" conservée !!
+
+// import { GenericList } from "./_modules/general/modeles/concreteClasses/GenericList.js";
+
+import { OrderedFullMatchPatternsList } from "./modeles/concreteClasses/OrderedFullMatchPatternsList.js";
 export class Main {
     public run(): void {
+        // const list1: GenericList<string> = new GenericList<string>();
+        // list1.setAllowNullElement(true);
+        // list1.addElements(Array("A",'B',"C",null));
 
+        // const list2: GenericList<string> = new GenericList<string>();
+        // list2.setAllowNullElement(true);
+        // list2.addElementsFromList(list1);
+        // list2.addElements(Array("D",'E',"F", null));        
+
+        // console.log(list2);
+
+        const pl: OrderedFullMatchPatternsList = new OrderedFullMatchPatternsList();
+        console.log(pl);
     }        
 
 }
---
+++ _src/app/modeles/index.ts
@@ -0,0 +1,4 @@
+export * from './abstracts';
+export * from './concreteClasses';
+export * from './interfaces';
+export * from './types';
